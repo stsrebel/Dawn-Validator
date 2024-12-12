5aed60668a9e08976e8602a3090e0a6b7856c1fe
fujiocrypto <120996626+stsrebel@users.noreply.github.com>
2024-12-12 22:01:50 +0700

    Add files via upload

---
+++ accounts.js
@@ -0,0 +1,9 @@
+// accounts.js
+module.exports = [
+  {
+    appid:"appid2",
+    email: "email_2@gmail.com",
+    token: "token2",
+  },
+];
+// Add more accounts as needed and replace templates by your accounts
---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  useProxy: false, // Set to true if you want to use proxies, false otherwise | Sử dụng proxy hay không (true/false)
+  restartDelay: 180, // Delay in seconds before restarting the process | Thời gian gửi ping(giây)
+};
+
+module.exports = config;
